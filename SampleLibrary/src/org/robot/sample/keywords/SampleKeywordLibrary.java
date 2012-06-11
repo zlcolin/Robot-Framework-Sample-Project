ef6627685de3bd46791e623c2d2caaf8c769756d
Thomas Jaspers <tritom42@googlemail.com>
2012-06-11 10:33:15 +0200

    All keywords added

--- SampleLibrary/src/org/robot/sample/keywords/SampleKeywordLibrary.java
+++ SampleLibrary/src/org/robot/sample/keywords/SampleKeywordLibrary.java
@@ -23,4 +23,43 @@ public class SampleKeywordLibrary {
     public void createAnEmptyStack() {
         testStack = new Stack<String>();
     }
+    
+  
+    /**
+     * Keyword-method to add an element to the stack.
+     * @param element The element
+     */
+    public void addAnElement(String element) {
+        testStack.push(element);
+    }
+ 
+    /**
+     * Keyword-method to remove the last element from the stack.
+     */
+    public void removeLastElement() {
+        testStack.pop();
+    }
+ 
+    /**
+     * Keyword-method to search for an element position.
+     * @param element The element
+     * @param pos The expected position
+     */
+    public void elementShouldBeAtPosition(String element, int pos) 
+            throws Exception {
+        if (testStack.search(element) != pos) {
+            throw new Exception("Wrong position: " + testStack.search(element));
+        }
+    }
+ 
+    /**
+     * Keyword-method to check the last element in the stack.
+     * @param result Expected resulting element
+     */
+    public void theLastElementShouldBe(String result) throws Exception {
+        String element = testStack.pop();
+        if (!result.equals(element)) {
+            throw new Exception("Wrong element: " + element);
+        }
+    }
 }
